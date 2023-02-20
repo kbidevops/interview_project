02bed1bb70f0bf6615603d3d0f49fb1482be03f8
yhdchoi <kbidevops@gmail.com>
2023-02-20 23:13:57 +0900

    Initial update

---
+++ securityBackend/src/main/java/com/kbi/sys/securityBackend/SecurityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kbi.sys.securityBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityBackendApplication.class, args);
+	}
+
+}
